d44edfe48bf006d0ab25198213f5fba893fd17f4
Fabian Marz <fabian@netzstrategen.com>
2018-01-29 17:27:58 +0100

    Added support for CLI arguments. (#2)

--- tasks/fractal.js
+++ tasks/fractal.js
@@ -2,7 +2,7 @@
 
 module.exports = (gulp, $, pkg) => {
   const path = require('path');
-  const fractal = require(path.resolve(pkg.fractalConfigPath));
+  const fractal = require(path.resolve(pkg.gulpPaths.fractalConfig));
   // @task: Start Fractal server.
   start: {
     const task = () => {
