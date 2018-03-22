a3b0e6fde16ca102a463bede07f4cf8c347e48f0
Fabian Marz <fabian@netzstrategen.com>
2018-03-22 17:50:59 +0100

    Added images are not build in default task. (#5)

--- tasks/default.js
+++ tasks/default.js
@@ -5,6 +5,7 @@ module.exports = (gulp, $, pkg) => {
   const watch = (callback) => {
     // Fractal automatically detects existing server instance.
     $.livereload.listen();
+    gulp.watch(pkg.gulpPaths.images.src, gulp.series('images'));
     gulp.watch(pkg.gulpPaths.styles.src, gulp.series('styles'));
     gulp.watch(pkg.gulpPaths.scripts.src, gulp.series('scripts'));
     gulp.watch(pkg.gulpPaths.templates, (files) => {
@@ -16,6 +17,7 @@ module.exports = (gulp, $, pkg) => {
   // @task: Default. Start Fractal and watch for changes.
   gulp.task('default', gulp.series(gulp.parallel(
     'fractal:start',
+    'images',
     'styles',
     'scripts'
   ), watch));
