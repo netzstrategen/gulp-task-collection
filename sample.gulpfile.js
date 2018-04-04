2dbb8be47d8ef29f249cdf64d824f5e111d45879
Tom Hare <tom@netzstrategen.com>
2018-04-04 17:13:25 +0200

    Added documentation and sample gulpfile.

---
+++ sample.gulpfile.js
@@ -0,0 +1,8 @@
+/**
+ * Sample gulpfile.
+ * Gulp tasks declared in the gulp-task-collection module.
+ * Asset paths set in package.json.
+ */
+
+const gulp = require('gulp');
+require('@netzstrategen/gulp-task-collection')(gulp);
