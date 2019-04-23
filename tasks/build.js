45bec4db7fbcb308ca1fa536f5830626f676baef
Tom Hare <tom@netzstrategen.com>
2019-04-23 13:42:16 +0200

    Added task to copy font files to dist.

--- tasks/build.js
+++ tasks/build.js
@@ -3,6 +3,7 @@
 module.exports = (gulp, $, pkg) => {
   // @task: Build all static assets.
   gulp.task('build', gulp.series('clean', gulp.parallel(
+    'fonts',
     'images',
     'scripts',
     'styles'
@@ -10,6 +11,7 @@ module.exports = (gulp, $, pkg) => {
 
   // @task: Build and minify all static assets.
   gulp.task('build:production', gulp.series('clean', gulp.parallel(
+    'fonts',
     'images',
     'scripts:production',
     'styles:production'
