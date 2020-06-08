b23612ad96f9f9f8e0c805414cef69f86d5caeb3
Callum Bonnyman <callum@netzstrategen.com>
2020-06-08 16:33:13 +0200

    Fixed build will fail without if paths are not defined.

--- tasks/build.js
+++ tasks/build.js
@@ -1,19 +1,31 @@
 'use strict';
 
 module.exports = (gulp, $, pkg) => {
+  const buildTasks = [
+    typeof(pkg.gulpPaths.fonts) === 'undefined' ? '' : 'fonts',
+    typeof(pkg.gulpPaths.icons) === 'undefined' ? '' : 'icons',
+    typeof(pkg.gulpPaths.scripts) === 'undefined' ? '' : 'scripts',
+    typeof(pkg.gulpPaths.styles) === 'undefined' ? '' : 'styles'
+  ];
+
   // @task: Build all static assets.
-  gulp.task('build', gulp.series('clean', 'images', gulp.parallel(
-    'fonts',
-    'icons',
-    'scripts',
-    'styles'
-  )));
+  gulp.task('build', gulp.series(
+    'clean',
+    'images',
+    gulp.parallel(buildTasks.filter(Boolean))
+  ));
+
+  const productionBuildTasks = [
+    typeof(pkg.gulpPaths.fonts) === 'undefined' ? '' : 'fonts',
+    typeof(pkg.gulpPaths.icons) === 'undefined' ? '' : 'icons',
+    typeof(pkg.gulpPaths.scripts) === 'undefined' ? '' : 'scripts:production',
+    typeof(pkg.gulpPaths.styles) === 'undefined' ? '' : 'styles:production'
+  ];
 
   // @task: Build and minify all static assets.
-  gulp.task('build:production', gulp.series('clean', 'images', gulp.parallel(
-    'fonts',
-    'icons',
-    'scripts:production',
-    'styles:production'
-  )));
+  gulp.task('build:production', gulp.series(
+    'clean',
+    'images',
+    gulp.parallel(productionBuildTasks.filter(Boolean))
+  ));
 }
