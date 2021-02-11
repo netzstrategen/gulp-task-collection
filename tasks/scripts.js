b538fe477ef326e14c1178f9a3ba71e1bfd9a900
Theo Junge <39087186+theoshallburn@users.noreply.github.com>
2021-02-11 11:27:00 +0100

    Fixed sourcemaps are not generated. (#36)

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -9,18 +9,16 @@ module.exports = (gulp, $, pkg) => {
   function scriptTask(opts) {
     if (!pkg.gulpPaths.scripts.src) { return false }
     const options = getOptions($, pkg.gulpPaths.scripts.options, opts);
-    return gulp.src(pkg.gulpPaths.scripts.src)
+    return gulp.src(pkg.gulpPaths.scripts.src, { sourcemaps: options.sourcemaps })
       .pipe($.if(!options['fail-after-error'], $.plumber()))
       .pipe($.eslint())
       .pipe($.eslint.format())
       .pipe($.if(options['fail-after-error'], $.eslint.failAfterError()))
-      .pipe($.if(options.sourcemaps, $.sourcemaps.init()))
       .pipe($.babel({
         presets: [['@babel/preset-env', { modules: false }]],
       }))
       .pipe($.if(options.concat, $.concat(pkg.title.toLowerCase().replace(/[^a-z]/g,'') + '.js')))
-      .pipe($.if(options.sourcemaps, $.sourcemaps.write()))
-      .pipe(gulp.dest(pkg.gulpPaths.scripts.dest))
+      .pipe(gulp.dest(pkg.gulpPaths.scripts.dest, { sourcemaps: options.sourcemaps }))
       .pipe($.if(options.minify, $.uglifyEs.default()))
       .pipe($.if(options.minify, $.rename({ suffix: '.min' })))
       .pipe($.if(options.minify, gulp.dest(pkg.gulpPaths.scripts.dest)))
