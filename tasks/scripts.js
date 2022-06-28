455f272d9b827ec37c46058fb17afe28a48dd9b1
Italo Devoto Ramella <italodr@gmail.com>
2022-06-28 11:15:27 +0200

    Updated function to keep same structure on compiled files.

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -10,14 +10,49 @@ module.exports = (gulp, $, pkg) => {
   function scriptTask(opts) {
     if (!pkg.gulpPaths.scripts.src) { return false }
     const options = getOptions($, pkg.gulpPaths.scripts.options, opts);
+    const tmp = {};
 
     return gulp
       .src(pkg.gulpPaths.scripts.src)
       .pipe($.if(!options['fail-after-error'], $.plumber()))
-      .pipe($.named())
-      .pipe($.webpackStream(webpackConfig, $.webpack))
-      .pipe($.rename({ suffix: '.min' }))
+      .pipe($.if(!options.concat, $.named()))
+      .pipe(
+        $.if(
+          !options.concat,
+          $.rename(path => (tmp[path.basename] = path))
+        )
+      )
+      .pipe(
+        $.webpackStream(
+          {
+            ...webpackConfig,
+            optimization: {
+              minimize: false
+            }
+          },
+          $.webpack
+        )
+      )
+      .pipe($.rename(path => (path.dirname = tmp[path.basename].dirname)))
       .pipe(gulp.dest(pkg.gulpPaths.scripts.dest))
+      .pipe($.if(!options.concat && options.minify, $.named()))
+      .pipe(
+        $.if(
+          !options.concat && options.minify,
+          $.rename(path => (tmp[path.basename] = path))
+        )
+      )
+      .pipe($.if(options.minify, $.webpackStream(webpackConfig, $.webpack)))
+      .pipe(
+        $.if(
+          !options.concat && options.minify,
+          $.rename(path => {
+            path.dirname = tmp[path.basename]?.dirname || '.';
+            path.basename = `${path.basename}.min`;
+          })
+        )
+      )
+      .pipe($.if(options.minify, gulp.dest(pkg.gulpPaths.scripts.dest)))
       .pipe($.touchCmd());
   };
 
